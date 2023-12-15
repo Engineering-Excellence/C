0ff2f9fa48e7f45e1d2e1cfed369e78cc902aeb7
Kyle <prophet@hanyang.ac.kr>
2023-12-15 22:03:15 +0900

    함수 - 라이브러리 함수

---
+++ Concert/chapter07/lecture/math1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    printf("sin(π/2) = %f\n", sin(M_PI / 2));
+    printf("cos(π/2) = %f\n", cos(M_PI / 2));
+    printf("tan(π/2) = %f\n", tan(M_PI / 2));
+    printf("ln(10) = %f\n", log(10));
+    printf("log10(100) = %f\n", log10(100));
+    printf("exp(10) = %f\n", exp(10));
+
+    return 0;
+}
---
+++ Concert/chapter07/lecture/random.c
@@ -0,0 +1,22 @@
+/* 의사 난수 생성 프로그램 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// n개의 난수를 화면에 출력한다.
+void get_random(int n) {
+    for (int i = 0; i < n; ++i) {
+        printf("%11d\n", rand());
+    }
+}
+
+int main(void) {
+    /*
+        일반적으로 난수 발생기의 시드(seed)를 현재 시간으로 설정한다.
+        현재 시간은 수행할 때마다 달라지기 때문이다.
+     */
+    srand((unsigned) time(NULL));
+    get_random(10);
+
+    return 0;
+}
