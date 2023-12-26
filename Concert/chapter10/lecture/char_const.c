268d7d7d737c661918cb46b1dfc5ed0511494cd6
Kyle <prophet@hanyang.ac.kr>
2023-12-26 19:13:50 +0900

    문자와 문자열 - 문자 표현 방법

---
+++ Concert/chapter10/lecture/char_const.c
@@ -0,0 +1,11 @@
+/* 문자 상수 */
+#include <stdio.h>
+
+int main(void) {
+    char code1 = 'A';   // 문자 A를 나타낸다.
+    char code2 = 65;
+
+    printf("code1 = %c, code2 = %c", code1, code2);
+
+    return 0;
+}
