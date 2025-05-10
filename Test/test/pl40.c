bbe03230052d231461bf0f3c6f4df8a3295f6b2a
Kyle <propheta@yonsei.ac.kr>
2025-05-10 19:33:24 +0900

    리눅스의 기본 개념과 프로그램 작성 - 두 개 이상의 소스 코드로 하나의 실행 파일 만들기

--- Test/test/pl40.c
+++
@@ -1,8 +0,0 @@
-﻿#include <stdio.h>
-
-void main() {
-    int x = 0x15213F10 >> 4;    // 16진수 자리당 4비트이므로, 오른쪽으로 한 자리 쉬프트 -> x = 0x15213F1
-    char y = (char) x;  // char는 8비트이므로, 최하위 8비트만 추출 -> y = 0xF1 = 11110001 = -15
-    unsigned char z = (unsigned char) x;    // MSB를 부호가 아닌 숫자로 취급 -> z = 11110001 = 241
-    printf("%d, %u", y, z);
-}
