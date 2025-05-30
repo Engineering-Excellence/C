7a2550611c4350f4160e752a3fc20e59e680ddbb
Kyle <propheta@yonsei.ac.kr>
2025-05-30 18:07:35 +0900

    프로세스 다루기 (2): 동기화, 속성, 환경 변수 - getsid, setsid

---
+++ Linux/chapter08/lecture/ex08-09.c
@@ -0,0 +1,18 @@
+﻿#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    pid_t pid;
+
+    if ((pid = fork()) > 0) {
+        sleep(1);
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        printf("old session id: %d\n", getsid(0));
+        printf("new session id: %d\n", setsid());
+        sleep(600);
+    }
+
+    return EXIT_SUCCESS;
+}
