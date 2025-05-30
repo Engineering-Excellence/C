92e0b5270d4e3f6a124b0ea80c9b6f255b63eaec
Kyle <propheta@yonsei.ac.kr>
2025-05-30 18:06:55 +0900

    프로세스 다루기 (2): 동기화, 속성, 환경 변수 - getpgrp, getpgid, setpgrp, setpgid

---
+++ Linux/chapter08/lecture/ex08-07.c
@@ -0,0 +1,15 @@
+﻿#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * getpgrp, getpgid, setpgrp, setpgid
+ *
+ * getpgrp과 getpgid는 프로세스의 프로세스 그룹 식별 번호를 구하고, setpgrp과 setpgid는 프로세스의 프로세스 그룹 식별 번호를 변경한다.
+ */
+int main(void) {
+    printf("getpgrp(): %d\n", getpgrp());
+    printf("getpgid(0): %d\n", getpgid(0));
+    printf("getpgid(getpid()): %d\n", getpgid(getpid()));
+
+    return 0;
+}
